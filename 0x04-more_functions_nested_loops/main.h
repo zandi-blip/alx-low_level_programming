8967da71c8511ff7a0dbe35de6a5f303a5af3c83
zandi-blip <zandilegoge746@gmail.com>
2023-03-24 16:47:32 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+int _putchar(char C);
+int _isupper(int C);
+int _isdigit(int C);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void print_more_numbers(void);
+void print_lines(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
