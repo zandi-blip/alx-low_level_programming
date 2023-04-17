fce787604253bac846e880dcaab00132d197d92d
zandi-blip <zandilegoge746@gmail.com>
2023-04-17 12:21:29 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* main - prints the name of the file it was compiled from
+*
+* Return - Always (0)
+*/
+
 int main()
 {
 printf("Name of source file:%s\n", _FILE_);
-
 return (0);
 
 }
