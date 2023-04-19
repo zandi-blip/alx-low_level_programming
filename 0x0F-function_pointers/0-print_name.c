8bc1c176f143d937caad13266e80bc8c874a463a
zandi-blip <zandilegoge746@gmail.com>
2023-04-19 13:42:38 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <function_pointers.h>
+#include <stdio.h>
+
+/**
+* print_name -  prints a name
+* @name: string to add
+*
+* Return: Nothing.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name == Zan || f == Zan)
+return (0);
+
+
+f(name);
+}(
